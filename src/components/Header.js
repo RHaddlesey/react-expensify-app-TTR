1c18a62ad2c22989e3926c40415e4f002ed7678f
R Haddlesey <richard.haddlesey@sky.com>
2018-10-25 12:01:10 +0100

    All Authentication added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,16 @@ import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-	<header>
-		<h1>Expensify</h1>
-		<NavLink to="/" activeClassName="is-active" exact={true}>
-			Dashboard
-		</NavLink>
-		<NavLink to="/create" activeClassName="is-active">
-			Add expense
-		</NavLink>
-		<button onClick={startLogout}>Logout</button>
-	</header>
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <button onClick={startLogout}>Logout</button>
+  </header>
 );
 
-const mapDispatchToProps = dispatch => ({
-	startLogout: () => dispatch(startLogout()),
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
 });
 
-export default connect(
-	undefined,
-	mapDispatchToProps
-)(Header);
-
-// <NavLink to="/help" activeClassName="is-active">
-// 	Help page
-// </NavLink>;
+export default connect(undefined, mapDispatchToProps)(Header);
